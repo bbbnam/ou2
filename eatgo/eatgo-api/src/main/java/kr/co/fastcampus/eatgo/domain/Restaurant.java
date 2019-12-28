111c33cc6227f753ad5335cff2191ddf65063368
bbbnam <bbbnam@naver.com>
2019-12-28 21:11:29 +0900

    eatgo 프로젝트 #1
    가게목록, 가게 상세

---
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,30 @@
+package kr.co.fastcampus.eatgo.domain;
+
+public class Restaurant {
+    private final String name;
+    private final String address;
+    private final Long id;
+
+    public Restaurant(Long id, String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInformation() {
+        return name+" in " +address;
+    }
+
+    public Object getAddress() {
+        return address;
+    }
+
+
+}
---
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -0,0 +1,35 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.domain.Restaurant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+
+    @GetMapping("/restaurants")
+    public List<Restaurant> list(){
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+
+        return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id")Long id){
+        Restaurant restaurant = null;
+        if(id == 1004L){
+            restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        }
+        if(id == 2020L){
+            restaurant = new Restaurant(2020L, "Cyber Food", "Seoul");
+        }
+        return restaurant;
+    }
+}
