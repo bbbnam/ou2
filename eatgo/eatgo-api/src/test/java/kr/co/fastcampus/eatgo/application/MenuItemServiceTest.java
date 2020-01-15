248513345c416887eccac4768c157f9904db737e
bbbnam <bbbnam@naver.com>
2020-01-16 01:34:58 +0900

    eatgo 프로젝트 #8
    메뉴추가 1 (컨트롤러)

---
+++ eatgo/eatgo-api/src/test/java/kr/co/fastcampus/eatgo/application/MenuItemServiceTest.java
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.eatgo.application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MenuItemServiceTest {
+
+    private MenuItemService menuItemService;
+
+
+}
